f4f1609ea6333f9402c1d33e31d2bb3bb7311eb5
maggot_code <wowhuanhuan233@163.com>
2021-03-02 10:35:54 +0800

    feat：tool package
    1.tool package init
    2.tool package readme

---
+++ packages/tool/README.md
@@ -0,0 +1,8 @@
+<!--
+ * @Author: maggot-code
+ * @Date: 2021-03-02 09:22:55
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2021-03-02 09:23:10
+ * @Description: maggot utils tool
+-->
+# maggot utils tool
---
+++ packages/tool/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: maggot-code
+ * @Date: 2021-03-02 09:23:21
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2021-03-02 09:38:46
+ * @Description: maggot utils tool inlet
+ */
+module.exports = {}
