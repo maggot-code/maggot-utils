c6b36d9e0e91053551b11a694cab4a458bc60975
maggot_code <wowhuanhuan233@163.com>
2021-03-11 12:07:43 +0800

    Update common.js

--- packages/tool/common.js
+++ packages/tool/common.js
@@ -2,14 +2,14 @@
  * @Author: maggot-code
  * @Date: 2021-03-11 10:45:49
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-03-11 11:10:06
+ * @LastEditTime: 2021-03-11 12:07:36
  * @Description: maggot utils tool common
  */
 import Flakeid from 'flakeid';
 
 const flake = new Flakeid({
     mid: new Date().getTime(),
-    timeOffset: (new Date.getFullYear() - 1970) * 31536000 * 1000
+    timeOffset: (new Date().getFullYear() - 1970) * 31536000 * 1000
 })
 
 const hasNumber = num => typeof num === 'number';
