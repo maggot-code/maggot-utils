5f6a4f41601bbe24ece8e45c9720184f5402d9aa
maggot_code <wowhuanhuan233@163.com>
2021-03-11 16:22:47 +0800

    fix：add request，send，sendAll function
    1.新增了请求发送方法，request
    2.新增了向上抛出的请求调用方法，通过闭包方式保存axios实例对象和该次请求参数
    3.新增了向上抛出的批量请求调用方法，还没有实现；

--- packages/axios/index.js
+++ packages/axios/index.js
@@ -2,15 +2,22 @@
  * @Author: maggot-code
  * @Date: 2021-03-11 10:02:16
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-03-11 12:15:39
+ * @LastEditTime: 2021-03-11 14:17:14
  * @Description: maggot utils axios inlet
  */
-const setParam = require('./setParam');
-const interceptRequest = require('./interceptRequest');
-const interceptResponse = require('./interceptResponse');
+const setParam = require('./setParam').default;
+const interceptRequest = require('./interceptRequest').default;
+const interceptResponse = require('./interceptResponse').default;
 
-module.export = {
-    Params: setParam,
+const request = (axios, options) => axios(setParam(options));
+
+const send = (axios) => (options) => request(axios, options);
+
+const sendAll = (axios) => { }
+
+module.exports = {
     InterceptRequest: interceptRequest,
-    InterceptResponse: interceptResponse
+    InterceptResponse: interceptResponse,
+    Send: send,
+    SendAll: sendAll
 }
