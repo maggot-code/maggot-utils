3998780393676ce936614d54bbdd3f3808912e4d
maggot_code <wowhuanhuan233@163.com>
2021-03-11 11:32:08 +0800

    feat：init date tool package
    1.创建时间工具包

---
+++ packages/tool/date.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: maggot-code
+ * @Date: 2021-03-11 11:24:19
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2021-03-11 11:24:52
+ * @Description: maggot utils tool date
+ */
+export default {}
