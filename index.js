27130000cc1a9f1df99bd57baeab09dc951d2340
maggot_code <wowhuanhuan233@163.com>
2021-03-11 13:08:58 +0800

    fix：update import mode && export mode
    1.工具包导入方式修改
    2.工具包导出方式修改
    3.tool包拆解导出

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: maggot-code
  * @Date: 2021-03-02 09:17:11
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-03-02 09:42:51
+ * @LastEditTime: 2021-03-11 12:39:57
  * @Description: maggot utils inlet
  */
 const toolPackage = require('./packages/tool');
@@ -10,7 +10,7 @@ const routerPackage = require('./packages/router');
 const axiosPackage = require('./packages/axios');
 
 module.exports = {
-    MGtool: toolPackage,
     MGrouter: routerPackage,
-    MGaxios: axiosPackage
+    MGaxios: axiosPackage,
+    ...toolPackage
 }
