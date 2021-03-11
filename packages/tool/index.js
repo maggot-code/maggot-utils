c1d6fca0b65a21f9222f104b91fbf2c2665b2663
maggot_code <wowhuanhuan233@163.com>
2021-03-11 11:31:26 +0800

    fix：export tool package
    1.导出common工具包
    2.导出cache工具包
    3.导出date工具包

--- packages/tool/index.js
+++ packages/tool/index.js
@@ -2,7 +2,15 @@
  * @Author: maggot-code
  * @Date: 2021-03-02 09:23:21
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-03-02 09:38:46
+ * @LastEditTime: 2021-03-11 11:25:13
  * @Description: maggot utils tool inlet
  */
-module.exports = {}
+import common from './common';
+import cache from './cache';
+import date from './date';
+
+module.exports = {
+    ...common,
+    ...cache,
+    ...date
+}
