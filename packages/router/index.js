fbf2302da938180f52b5b435aaf4ad8e55cd203f
maggot_code <wowhuanhuan233@163.com>
2022-09-16 17:05:38 +0800

    Update index.js

--- packages/router/index.js
+++ packages/router/index.js
@@ -2,14 +2,13 @@
  * @Author: maggot-code
  * @Date: 2021-03-02 09:41:08
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-09-16 17:00:15
+ * @LastEditTime: 2022-09-16 17:05:24
  * @Description: maggot utils router inlet
  */
 const VueRouter = require('vue-router');
 const baseBeforeFunc = require('./beforeEach').default;
 const baseAfterFunc = require('./afterEach').default;
 
-console.log(VueRouter);
 const originalPush = VueRouter.prototype.push;
 VueRouter.prototype.push = function push(location) {
     return originalPush.call(this, location).catch(err => err)
